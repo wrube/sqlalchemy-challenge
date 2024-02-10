3dfc1bff86dbfd6ee000f54265e7a97ea6ead524
wrube <bjminwruck@gmail.com>
2024-02-11 04:59:52 +1000

    started solution

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,13 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from pathlib import Path
+
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,15 +15,21 @@
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+db_path = Path.cwd() / Path('./Resources/hawaii.sqlite')
+
+engine = create_engine(f"sqlite:///{db_path}")
 
 # Declare a Base using `automap_base()`
 
-# Use the Base class to reflect the database tables
+Base = automap_base()
 
+# Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
 
@@ -23,10 +37,25 @@
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def welcome():
+    """List all possible api routes
+    """
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
